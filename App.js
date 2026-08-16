8bfc91be03196ffd00edc3817a487dba18187bec
ani7singh <anitasingh2067@gmail.com>
2026-08-16 11:38:57 +0530

    jsx introduced basic

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-    const parent = React.createElement( "div",{ id: "parent" },[
-      React.createElement( "div",{ id: "child1" }, [
-        React.createElement( "h1", {id: "heading_1"}, "Heading1"),
-          React.createElement( "h2", {id: "heading_2"}, "Heading2")
-         ]),
-          React.createElement( "div",{ id: "child2" },[
-            React.createElement( "h1", {id: "heading_1"}, "Heading1"),
-            React.createElement( "h2", {id: "heading_2"}, "Heading2")
-         ]),
-        ]);
-      console.log(parent);
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+import React from "react";
+import { createRoot } from "react-dom/client"; 
+
+    const jsxHeading = (
+        <h1 className="head">
+            Namste React using JSX
+        </h1>
+    );
+    console.log(jsxHeading)
+    const root = createRoot(document.getElementById("root"));
+    root.render(jsxHeading);
