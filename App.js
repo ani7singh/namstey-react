a44cfb192b3295359c34bf7cb210ea8e5d1d45c3
ani7singh <anitasingh2067@gmail.com>
2026-08-16 20:07:39 +0530

    react component composition and functional component

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client"; 
 
-    const jsxHeading = (
+    const Title = () => (
         <h1 className="head">
             Namste React using JSX
         </h1>
     );
-    console.log(jsxHeading)
+    //component composition
+        const HeadingComponent = () => (
+            <div id="container">
+                <Title />
+                <h1 className="headingExample">React Functional component</h1>
+            </div>
+        )
     const root = createRoot(document.getElementById("root"));
-    root.render(jsxHeading);
+    root.render(<HeadingComponent />);
