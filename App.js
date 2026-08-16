e2ed18c78562e1e7a79e5da9969cd04a2f5cfd94
ani7singh <anitasingh2067@gmail.com>
2026-08-16 20:35:54 +0530

    component and jsx concept

--- App.js
+++ App.js
@@ -6,6 +6,11 @@ import { createRoot } from "react-dom/client";
             Namste React using JSX
         </h1>
     );
+        //used curly braces which uses return and mentioned below function give the same result
+        // const HeadingComponent1 = () => {
+        //     return <h1 className="headingExample">React Functional component</h1>
+        // }
+        //jsx uses round braces for muktiple lines
     //component composition
         const HeadingComponent = () => (
             <div id="container">
