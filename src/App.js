0ae46b7e32b167bd9516de1c4873c24def59e2c6
ani7singh <anitasingh2067@gmail.com>
2026-08-19 18:30:07 +0530

    html element comment added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import { createRoot } from "react-dom/client";
 import { Header } from "./components/Header";
 import Body from "./components/Body";
 import RestaurantCard from "./components/RestaurantCard";
-
+/**
+ *
+ * <div id="parent">
+ *      <div id="child">
+ *         <h1>I'm h1 tag</h1>
+ *         <h2>I'm h1 tag</h2>
+ *      </div>
+ * <div id="child2">
+ *         <h1>I'm h1 tag</h1>
+ *         <h2>I'm h1 tag</h2>
+ *      </div>
+ * </div>
+ *
+ * ReactElement(Object) => HTML(Browser Understands)
+ */
     const AppComponent = () =>{
         return  (
             <div className="app">
